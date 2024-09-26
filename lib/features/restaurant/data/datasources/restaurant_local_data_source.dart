11e927263b6f19cbd24c1cef9c7572a36baeef8b
bits-io <bits1933@gmail.com>
2024-09-26 14:57:53 +0700

    init

--- lib/features/restaurant/data/datasources/restaurant_local_data_source.dart
+++ lib/features/restaurant/data/datasources/restaurant_local_data_source.dart
@@ -1,13 +1,18 @@
 import 'dart:convert';
-import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import '../models/restaurant_model.dart';
 
 class RestaurantLocalDataSource {
   Future<List<RestaurantModel>> getRestaurantsFromJson() async {
-    final jsonString =
-        await rootBundle.loadString('json/local_restaurant.json');
-    final Map<String, dynamic> jsonMap = json.decode(jsonString);
-    final List restaurants = jsonMap['restaurants'];
-    return restaurants.map((json) => RestaurantModel.fromJson(json)).toList();
+    final response = await http.get(Uri.parse(
+        'https://raw.githubusercontent.com/bits-io/dicoding-restaurant/main/assets/json/local_restaurant.json'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonMap = json.decode(response.body);
+      final List restaurants = jsonMap['restaurants'];
+      return restaurants.map((json) => RestaurantModel.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load restaurants');
+    }
   }
 }
